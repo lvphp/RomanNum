cc1317cfcead0e8378a3926a9b702812d07ca56b
Tim Lytle <tim@timlytle.net>
2011-10-14 17:46:57 -0400

    Added fuller range of tests.

--- test/RomanNumTest.php
+++ test/RomanNumTest.php
@@ -16,7 +16,22 @@ class RomanNumTest extends PHPUnit_Framework_TestCase
     public function provider()
     {
         return array(
-          array('X', 10),
+            array('I', 1),
+            array('II', 2),
+            array('III', 3),
+            array('IV', 4),
+            array('V', 5),
+            array('VI', 6),
+            array('VII', 7),
+            array('VIII', 8),
+            array('IX', 9),
+            array('XXV', 25),
+            array('XVII', 17),
+            array('XIV', 14),
+            array('XII', 12),
+            array('XIX', 19),
+            array('XXXIV', 34),
+            array('XXXIX', 39),
         );
     }
 }
