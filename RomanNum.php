a47887720ba03f5c04b50ce5b1b14474233b13a9
Tim Lytle <tim@timlytle.net>
2011-11-14 18:57:22 -0500

    Passing Funtion

--- RomanNum.php
+++ RomanNum.php
@@ -1,8 +1,45 @@
 <?php
 class RomanNum
 {
-    function toInt($numeral)
+    public function toInt($numeral)
     {
-        return $numeral;
+        $sum = 0;
+        $high = 0;
+        //reverse and split into characters
+        foreach(str_split(strrev($numeral)) as $pos => $chr){
+            //track the higest value
+            if($high < $this->getValue($chr)){
+                $high = $this->getValue($chr);
+            }
+            
+            //are there any pre values
+            if($pos == 0){
+                $sum += $this->getValue($chr);
+                continue;
+            }
+            
+            //is this a smaller value
+            if($high > $this->getValue($chr)){
+                $sum -= $this->getValue($chr);
+            } else {
+                $sum += $this->getValue($chr);
+            }
+        }
+        
+        return $sum;
+    }
+    
+    protected function getValue($numeral)
+    {
+        switch(strtolower($numeral)){
+            case 'i':
+                return 1;
+            case 'v':
+                return 5;
+            case 'x':
+                return 10;
+            default:
+                return 0;
+        }
     }
 }
